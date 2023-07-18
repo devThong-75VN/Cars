461eaa510752ea5f36f5f6f306e1ced70445e93a
linh-Artisan <minhthongk33@gmail.com>
2023-07-18 08:59:21 +0700

    migration

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,6 +12,6 @@ class Category extends Model
     protected $fillable = ['name', 'desc'];
     public function products()
     {
-        return $this->hasMany(Product::class)
+        return $this->hasMany(Product::class);
     }
 }
