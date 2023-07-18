a343c962a21530c6e6801f8a7612a8e32d7960e1
linh-Artisan <139750362+linh-Artisan@users.noreply.github.com>
2023-07-18 08:26:38 +0700

    migration

---
+++ database/migrations/2023_07_18_005906_create_oders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('oders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',128);
+            $table->string('desc',1024);
+            $table->string('status');
+            $table->string('receiver');
+            $table->unsignedBigInteger('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('oders');
+    }
+};
